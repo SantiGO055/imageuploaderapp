4f52f84fc23ba64c0745d8459f036af0489c2ce8
SantiGO055 <santigonzalez05@gmail.com>
2023-04-17 12:10:17 -0300

    .

--- src/app/dnd.directive.ts
+++ src/app/dnd.directive.ts
@@ -6,31 +6,33 @@ import { Directive, HostListener, HostBinding, EventEmitter, Output } from '@ang
 export class DndDirective {
 
 
-  @HostBinding('class.fileover') fileOver: boolean | undefined;
+  @HostBinding('class.fileover')
+  fileOver!: boolean;
   @Output() fileDropped = new EventEmitter<any>();
 
   constructor() { }
   @HostListener('dragover', ['$event']) onDragOver(evt: Event) {
     evt.preventDefault();
     evt.stopPropagation();
+    this.fileOver = true;
   }
 
   @HostListener('dragleave', ['$event']) public onDragLeave(evt: Event) {
     evt.preventDefault();
     evt.stopPropagation();
-
+    this.fileOver = false;
     console.log('Drag Leave');
   }
 
   @HostListener('drop', ['$event']) public ondrop(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.fileOver = false;
+
 
     // const files = (evt.target as HTMLInputElement).files[0];
     const files = evt.dataTransfer.files;
     console.log(files)
-
+    this.fileOver = false;
 
     if (files.length > 0 && files != undefined) {
       console.log(`You dropped ${files.length} files.`);
