cdf57e1618527eb3b0647f1ce2bb0655efc4b741
SantiGO055 <santigonzalez05@gmail.com>
2023-04-18 13:56:40 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,20 +2,5 @@
 export const environment = {
   production: false,
 
-  apiKey: "AIzaSyBPJK0wgdh0K1cNqh-3ReFW29_iBklbMiY",
-  authDomain: "imageuploaderapp-537b8.firebaseapp.com",
-  projectId: "imageuploaderapp-537b8",
-  storageBucket: "imageuploaderapp-537b8.appspot.com",
-  messagingSenderId: "360385508704",
-  appId: "1:360385508704:web:a68eaf671a4a8ae3c35a05",
-  measurementId: "G-YETE3JCDJ1"
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyBPJK0wgdh0K1cNqh-3ReFW29_iBklbMiY",
-  //   authDomain: "imageuploaderapp-537b8.firebaseapp.com",
-  //   projectId: "imageuploaderapp-537b8",
-  //   storageBucket: "imageuploaderapp-537b8.appspot.com",
-  //   messagingSenderId: "360385508704",
-  //   appId: "1:360385508704:web:a68eaf671a4a8ae3c35a05",
-  //   measurementId: "G-YETE3JCDJ1"
-  // }
+
 };
