4a58902cc344ed6d849e2f544d08f554d84075dc
SantiGO055 <santigonzalez05@gmail.com>
2023-04-18 13:53:28 -0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16a2Npemx0cWZ2aGJqaHNjdWhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA1NDU2MjMsImV4cCI6MTk5NjEyMTYyM30.PZGmY-10l_fA5r91YMqbUJeAWvpA3IytL-uF4hAbssM',
+  supabaseUrl: 'https://mzkcizltqfvhbjhscuhn.supabase.co',
 
+  apiKey: "AIzaSyBPJK0wgdh0K1cNqh-3ReFW29_iBklbMiY",
+  authDomain: "imageuploaderapp-537b8.firebaseapp.com",
+  projectId: "imageuploaderapp-537b8",
+  storageBucket: "imageuploaderapp-537b8.appspot.com",
+  messagingSenderId: "360385508704",
+  appId: "1:360385508704:web:a68eaf671a4a8ae3c35a05",
+  measurementId: "G-YETE3JCDJ1"
 
 };
